0830b29cece6c4afba64963ad4ca7c0ae0f74948
Neeraj Gupta <na1993194@gmail.com>
2025-02-02 16:31:14 +0530

    feat: router

--- server/routes/faq.routes.js
+++ server/routes/faq.routes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
 
+const { getAllFAQs, createFAQ, updateFAQ, removeFAQ } = require('../controllers/faqs.controller');
+
+
+router.get('/', getAllFAQs);
+router.post('/create', createFAQ);
+router.put('/update/:id', updateFAQ);
+router.delete('/delete/:id', removeFAQ);
+
+
+module.exports = router;
