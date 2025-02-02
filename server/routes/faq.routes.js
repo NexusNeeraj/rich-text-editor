b119be975dd61cbb636fdbd4bdbf3300a222bd41
Neeraj Gupta <na1993194@gmail.com>
2025-02-02 19:43:06 +0530

    fix:fixesd admin only routes

--- server/routes/faq.routes.js
+++ server/routes/faq.routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { isAdmin } = require('../middlewares/auth');
 
 const { getAllFAQs, createFAQ, updateFAQ, removeFAQ } = require('../controllers/faqs.controller');
 
 
 router.get('/', getAllFAQs);
 router.post('/create', createFAQ);
-router.put('/update/:id', updateFAQ);
-router.delete('/delete/:id', removeFAQ);
+router.put('/update/:id', isAdmin, updateFAQ);
+router.delete('/delete/:id', isAdmin, removeFAQ);
 
 
 module.exports = router;
